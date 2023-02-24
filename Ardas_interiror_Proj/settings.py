ebade9d7b149d4aaf975b4023910e66bcd80d6cd
armaan786-web <52047093+armaan786-web@users.noreply.github.com>
2023-02-24 22:23:02 +0530

    new

--- Ardas_interiror_Proj/settings.py
+++ Ardas_interiror_Proj/settings.py
@@ -66,10 +66,22 @@ WSGI_APPLICATION = 'Ardas_interiror_Proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ArdasBackend',
+        'USER': 'root',
+        # 'PASSWORD': 'root',
+        # 'PASSWORD': 'Manish@1999',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
     }
 }
 
@@ -117,6 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
