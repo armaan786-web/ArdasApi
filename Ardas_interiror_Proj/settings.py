4eef69233231d0bd63ad7a2a7d04b0da6de79547
armaan786-web <52047093+armaan786-web@users.noreply.github.com>
2023-02-24 23:24:25 +0530

    adding database

--- Ardas_interiror_Proj/settings.py
+++ Ardas_interiror_Proj/settings.py
@@ -67,25 +67,15 @@ WSGI_APPLICATION = 'Ardas_interiror_Proj.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'ArdasBackend',
-#         'USER': 'root',
-#         # 'PASSWORD': 'root',
-#         # 'PASSWORD': 'Manish@1999',
-#         # 'HOST': '127.0.0.1',
-#         # 'PORT': '3306',
-#     }
-# }
+
 
 
 # Password validation
